cfd1acfa91177156cab1f5165fda26877ff20e58
José Miguel González Aguilera <jmgaguilera@gmail.com>
2016-08-18 20:06:30 +0200

    genero una versión nueva con los enlaces externos

--- output_html/labels.pl
+++ output_html/labels.pl
@@ -2,19 +2,15 @@
 # Associate labels original text with physical files.
 
 
+$key = q/ch:chardet/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:emplib/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig08/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm02/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm011/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig01/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -22,19 +18,7 @@ $key = q/ch:refactorizar/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figu03/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig03/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig09/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm01/;
+$key = q/sec:shell/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -42,111 +26,35 @@ $key = q/figm06/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ch:instalacion/;
+$key = q/fig01/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu04/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:cadenas/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm00/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm09/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig05/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:primerprograma/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:xml/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig06/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu01/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig04/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:chardet/;
-$external_labels{$key} = "$URL/" . q|node12.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:comprensiones/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu05/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu02/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:novedades/;
-$external_labels{$key} = "$URL/" . q|node2.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:tiposdedato/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu08/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu07/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm04/;
-$external_labels{$key} = "$URL/" . q|node3.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:serializacion/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/figm010/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig07/;
+$key = q/figm08/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ch:emplib/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:shell/;
+$key = q/figu00/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sc:search_path/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
+$key = q/fig02/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figu09/;
+$key = q/ch:tiposdedato/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:xml/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm07/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -154,7 +62,55 @@ $key = q/figm03/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm07/;
+$key = q/figu07/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu03/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu02/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:instalacion/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm02/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig09/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu01/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig07/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:cadenas/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm01/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm011/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:primerprograma/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm04/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -162,7 +118,39 @@ $key = q/ch:clases/;
 $external_labels{$key} = "$URL/" . q|node8.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig02/;
+$key = q/fig04/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:novedades/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm05/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu06/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm00/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig05/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu05/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu08/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu09/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -170,11 +158,11 @@ $key = q/ch:migcod/;
 $external_labels{$key} = "$URL/" . q|node14.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm08/;
+$key = q/figm09/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm05/;
+$key = q/figu04/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
@@ -182,12 +170,24 @@ $key = q/ch:metesp/;
 $external_labels{$key} = "$URL/" . q|node15.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figu06/;
+$key = q/ch:serializacion/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig06/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figu00/;
+$key = q/fig03/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sc:search_path/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:comprensiones/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 1;
@@ -197,8 +197,104 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
+$key = q/figm01/;
+$external_latex_labels{$key} = q|11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:servicios_web/;
+$external_latex_labels{$key} = q|14|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:ficheros/;
+$external_latex_labels{$key} = q|11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm011/;
+$external_latex_labels{$key} = q|21|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu01/;
+$external_latex_labels{$key} = q|23|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig07/;
+$external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:cadenas/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:iteravan/;
+$external_latex_labels{$key} = q|8|; 
+$noresave{$key} = "$nosave";
+
 $key = q/figm02/;
 $external_latex_labels{$key} = q|12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig09/;
+$external_latex_labels{$key} = q|9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu07/;
+$external_latex_labels{$key} = q|29|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm03/;
+$external_latex_labels{$key} = q|13|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm07/;
+$external_latex_labels{$key} = q|17|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu03/;
+$external_latex_labels{$key} = q|25|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:instalacion/;
+$external_latex_labels{$key} = q|0|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu02/;
+$external_latex_labels{$key} = q|24|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig01/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm010/;
+$external_latex_labels{$key} = q|20|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figu00/;
+$external_latex_labels{$key} = q|22|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm08/;
+$external_latex_labels{$key} = q|18|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig02/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:tiposdedato/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:xml/;
+$external_latex_labels{$key} = q|12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:shell/;
+$external_latex_labels{$key} = q|0.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm06/;
+$external_latex_labels{$key} = q|16|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig08/;
@@ -209,20 +305,40 @@ $key = q/ch:refactorizar/;
 $external_latex_labels{$key} = q|10|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm011/;
-$external_latex_labels{$key} = q|21|; 
+$key = q/ch:cierres/;
+$external_latex_labels{$key} = q|6|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig01/;
-$external_latex_labels{$key} = q|1|; 
+$key = q/ch:chardet/;
+$external_latex_labels{$key} = q|15|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm01/;
-$external_latex_labels{$key} = q|11|; 
+$key = q/ch:emplib/;
+$external_latex_labels{$key} = q|16|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig09/;
+$key = q/ch:pruebas_unitarias/;
 $external_latex_labels{$key} = q|9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sc:search_path/;
+$external_latex_labels{$key} = q|1.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:comprensiones/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:expresionesregulares/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig06/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:serializacion/;
+$external_latex_labels{$key} = q|13|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig03/;
@@ -233,188 +349,72 @@ $key = q/sec:httplib2/;
 $external_latex_labels{$key} = q|14.5|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figu03/;
-$external_latex_labels{$key} = q|25|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm09/;
-$external_latex_labels{$key} = q|19|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:ficheros/;
-$external_latex_labels{$key} = q|11|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:cadenas/;
-$external_latex_labels{$key} = q|4|; 
+$key = q/sec:leer_ficheros/;
+$external_latex_labels{$key} = q|11.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/figu04/;
 $external_latex_labels{$key} = q|26|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ch:instalacion/;
-$external_latex_labels{$key} = q|0|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm06/;
-$external_latex_labels{$key} = q|16|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm00/;
-$external_latex_labels{$key} = q|10|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig04/;
-$external_latex_labels{$key} = q|4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu01/;
-$external_latex_labels{$key} = q|23|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig06/;
-$external_latex_labels{$key} = q|6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:xml/;
-$external_latex_labels{$key} = q|12|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:servicios_web/;
-$external_latex_labels{$key} = q|14|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:pruebas_unitarias/;
-$external_latex_labels{$key} = q|9|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig05/;
-$external_latex_labels{$key} = q|5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:primerprograma/;
-$external_latex_labels{$key} = q|1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:novedades/;
-$external_latex_labels{$key} = q|-1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu02/;
-$external_latex_labels{$key} = q|24|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:chardet/;
-$external_latex_labels{$key} = q|15|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:comprensiones/;
-$external_latex_labels{$key} = q|3|; 
+$key = q/ch:metesp/;
+$external_latex_labels{$key} = q|B|; 
 $noresave{$key} = "$nosave";
 
 $key = q/figu05/;
 $external_latex_labels{$key} = q|27|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm04/;
-$external_latex_labels{$key} = q|14|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:emplib/;
-$external_latex_labels{$key} = q|16|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig07/;
-$external_latex_labels{$key} = q|7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm010/;
-$external_latex_labels{$key} = q|20|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:serializacion/;
-$external_latex_labels{$key} = q|13|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu08/;
-$external_latex_labels{$key} = q|30|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:tiposdedato/;
-$external_latex_labels{$key} = q|2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu07/;
-$external_latex_labels{$key} = q|29|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig02/;
-$external_latex_labels{$key} = q|2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm07/;
-$external_latex_labels{$key} = q|17|; 
+$key = q/fig05/;
+$external_latex_labels{$key} = q|5|; 
 $noresave{$key} = "$nosave";
 
 $key = q/figu09/;
 $external_latex_labels{$key} = q|31|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm03/;
-$external_latex_labels{$key} = q|13|; 
+$key = q/figu08/;
+$external_latex_labels{$key} = q|30|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ch:expresionesregulares/;
-$external_latex_labels{$key} = q|5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:shell/;
-$external_latex_labels{$key} = q|0.7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sc:search_path/;
-$external_latex_labels{$key} = q|1.4|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:clases/;
-$external_latex_labels{$key} = q|7|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figu00/;
-$external_latex_labels{$key} = q|22|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:cierres/;
-$external_latex_labels{$key} = q|6|; 
-$noresave{$key} = "$nosave";
-
-$key = q/ch:iteravan/;
-$external_latex_labels{$key} = q|8|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:leer_ficheros/;
-$external_latex_labels{$key} = q|11.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/figm08/;
-$external_latex_labels{$key} = q|18|; 
+$key = q/figm09/;
+$external_latex_labels{$key} = q|19|; 
 $noresave{$key} = "$nosave";
 
 $key = q/ch:migcod/;
 $external_latex_labels{$key} = q|A|; 
 $noresave{$key} = "$nosave";
 
-$key = q/ch:metesp/;
-$external_latex_labels{$key} = q|B|; 
+$key = q/figm05/;
+$external_latex_labels{$key} = q|15|; 
 $noresave{$key} = "$nosave";
 
 $key = q/figu06/;
 $external_latex_labels{$key} = q|28|; 
 $noresave{$key} = "$nosave";
 
-$key = q/figm05/;
-$external_latex_labels{$key} = q|15|; 
+$key = q/figm00/;
+$external_latex_labels{$key} = q|10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:novedades/;
+$external_latex_labels{$key} = q|-1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:clases/;
+$external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig04/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ch:primerprograma/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figm04/;
+$external_latex_labels{$key} = q|14|; 
 $noresave{$key} = "$nosave";
 
 1;
